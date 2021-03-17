cbc428e84235bfdfd2937d5bd0269daa05105753
Victor <rusuvictor30@gmail.com>
2021-03-16 22:57:16 -0400

    Init

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Login With Google
+ * Description: This plugin will add a Login with Google Button
+ * Plugin URI: https://vicodemedia.com
+ * Author: Victor Rusu
+ * Version: 0.0.1
+**/
+
+//* Don't access this file directly
+defined( 'ABSPATH' ) or die();
+
